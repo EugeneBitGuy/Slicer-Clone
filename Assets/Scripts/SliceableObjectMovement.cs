b94a92dddc75cc6b74ef82d93e119b009878d64f
Yevhenii Postoienko <postoinko@gmail.com>
2023-03-29 21:50:25 +0300

    Fix. Linear object movement

--- Assets/Scripts/SliceableObjectMovement.cs
+++ Assets/Scripts/SliceableObjectMovement.cs
@@ -5,39 +5,23 @@ using UnityEngine;
 
 public class SliceableObjectMovement : MonoBehaviour
 {
-    private const float TwoPie = Mathf.PI * 2f;
-    private const float MovementFactorDivisor = 2f;
-    private const float MovementFactorOffset = 1f;
-    
-    
     [SerializeField] private Transform startPoint;
     [SerializeField] private Transform endPoint;
 
-    [SerializeField] private float movementPeriod = 5f;
-
-    private float _startTime;
-
+    [SerializeField] private float movementSpeed = 5f;
+    
     private void Start()
     {
         transform.position = startPoint.position;
-        _startTime = Time.time;
     }
 
     private void Update()
     {
-        if(movementPeriod <= Mathf.Epsilon)
-            return;
-        
-        float elapsedTime = Time.time - _startTime;
-
-        float numberOfFullCycles = elapsedTime / movementPeriod;
-
-        float sinValue = Mathf.Sin(numberOfFullCycles * TwoPie);
-
-        float movementFactor = (sinValue + MovementFactorOffset) / MovementFactorDivisor;
-
-        Vector3 offset = (endPoint.position - startPoint.position) * movementFactor;
-
-        transform.position = startPoint.position + offset;
+        transform.position = Vector3.MoveTowards
+        (
+            transform.position, 
+            endPoint.position,
+            Time.deltaTime * movementSpeed
+        );
     }
 }
